d3527c969ac0e2d5955f536b6d673b500efee845
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-03-02 11:09:49 +0530

    Update and rename isSubsequence-topdwon.java to isSubsequence.java

--- isSubsequence.java
+++ isSubsequence.java
@@ -1,4 +1,5 @@
 //leetcode.com/problems/is-subsequence/
+//1.Topdown
 class Solution {
     int[][] dp;
     public boolean isSubsequence(String s, String t) {
@@ -33,5 +34,31 @@ class Solution {
             return dp[m][n]=1+LCS(s,t,m-1,n-1);
         }
         return dp[m][n]=Math.max(LCS(s,t,m,n-1),LCS(s,t,m-1,n));
+    }
+}
+
+//2.efficient approach O(n)
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i_s=0;
+        int i_t=0;
+        int m=s.length();
+        
+        if(m==0){
+            return true;
+        }
+        int n=t.length();
+       
+        for(int i=0;i<n;i++){
+            if(s.charAt(i_s)==t.charAt(i_t)){
+                i_s++;
+                if(i_s==m){
+                    return true;
+                }
+            }
+            i_t++;
+        }
+        
+        return false;
     }
 }
