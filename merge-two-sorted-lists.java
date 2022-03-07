6f25324fa3c10213151670800bfaa15801adf5a4
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-03-07 20:11:53 +0530

    Update merge-two-sorted-lists.java

--- merge-two-sorted-lists.java
+++ merge-two-sorted-lists.java
@@ -9,6 +9,11 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//Approach1: 1ms
+//Approac2:0 ms
+
+//Approach1:
 class Solution {
     
     ListNode Next=new ListNode(-1);
@@ -46,4 +51,30 @@ class Solution {
         
         return solve(list1,list2);
     }
+}
+//**************************************************************************************************************************************************************************
+//Approach2:
+class Solution {
+    
+    
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        if(list1==null){
+            return list2;
+        }
+        if(list2==null){
+           return list1;
+        }
+        
+        if(list1.val<list2.val){
+            list1.next=mergeTwoLists(list1.next,list2);
+            return list1;
+        }
+        list2.next=mergeTwoLists(list1,list2.next);
+        return list2;
+        
+        
+    }
+    
+   
 }
