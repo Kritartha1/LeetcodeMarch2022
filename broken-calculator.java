cdcc824c4e3f807489f03402a8b7d337bb673c10
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-03-24 00:20:18 +0530

    Create broken-calculator.java

---
+++ broken-calculator.java
@@ -0,0 +1,18 @@
+//leetcode.com/problems/broken-calculator/
+class Solution {
+    
+    public int brokenCalc(int startValue, int target) {
+        int ans=0;
+        while(target>startValue){
+            ++ans;
+            if((target&1)!=0){
+                ++target;
+            }
+            else{
+                target/=2;
+            }
+        }
+        
+        return ans+startValue-target;
+    }
+}
